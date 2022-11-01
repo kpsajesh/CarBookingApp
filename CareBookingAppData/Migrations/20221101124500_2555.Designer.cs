6fd9aa30e65a7d1e68bfeaa4525c4daf1f4f637f
kpsajesh <sajeshkumarkp@gmail.com>
2022-11-01 13:17:32 +0000

    Upgraded to Dotnet 6 framework and corresponding libraries

--- CareBookingAppData/Migrations/20221101124500_2555.Designer.cs
+++ CareBookingAppData/Migrations/20221101124500_2555.Designer.cs
@@ -4,14 +4,16 @@ using CareBookingAppData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CareBookingAppData.Migrations
 {
     [DbContext(typeof(CarBookingAppDbContext))]
-    partial class CarBookingAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221101124500_2555")]
+    partial class _2555
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CareBookingAppData/Migrations/20221101124500_2555.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CareBookingAppData.Migrations
+{
+    public partial class _2555 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarModels_Makes_makeId",
+                table: "CarModels");
+
+            migrationBuilder.RenameColumn(
+                name: "makeId",
+                table: "CarModels",
+                newName: "MakeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CarModels_makeId",
+                table: "CarModels",
+                newName: "IX_CarModels_MakeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarModels_Makes_MakeId",
+                table: "CarModels",
+                column: "MakeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarModels_Makes_MakeId",
+                table: "CarModels");
+
+            migrationBuilder.RenameColumn(
+                name: "MakeId",
+                table: "CarModels",
+                newName: "makeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CarModels_MakeId",
+                table: "CarModels",
+                newName: "IX_CarModels_makeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarModels_Makes_makeId",
+                table: "CarModels",
+                column: "makeId",
+                principalTable: "Makes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
